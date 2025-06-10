57d18b0d2e296b3a322a5de956b5561302dda25a
Bibhut <erbthakuri@gmail.com>
2025-06-10 10:27:23 +0545

    Initial Commit

---
+++ HIT/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HIT
+//
+//  Created by Vanila Tech Bibhut on 6/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HIT/HITApp.swift
@@ -0,0 +1,17 @@
+//
+//  HITApp.swift
+//  HIT
+//
+//  Created by Vanila Tech Bibhut on 6/10/25.
+//
+
+import SwiftUI
+
+@main
+struct HITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
